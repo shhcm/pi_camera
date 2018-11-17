cad011b58fa4efe32957dd71e5161d4d7b053dda
shhcm <outlaw@gmx-topmail.de>
2018-11-17 22:03:33 +0100

    Keep only files of last n days, clean up the rest.

---
+++ clean_ftp_dir.py
@@ -0,0 +1,61 @@
+import traceback
+#import ftputil
+from ftplib import FTP
+# Try ftputil instead
+
+ftp_base_dir="/ASMT-2115-01/camera_0/"
+max_dirs=7
+
+
+#host = ftputil.FTPHost("FRITZ.NAS", "ftpuser", "?NOfritzuserftp?")
+#mypath = '/ASMT-2115-01/camera_0/20181018'
+#host.chdir(mypath)
+#names = host.listdir(host.curdir)
+#for name in names:
+#  if host.path.isfile(name):
+#    host.remove(name)
+
+
+#print('Closing FTP connection')
+#host.close()
+
+try:
+  ftp = FTP("HOST", "USER", "PASS")
+  ftp_del = FTP("HOST", "USER", "PASS") # Instance ftp blocks during LIST operation.
+  dir_list=[]
+
+  try:
+    ftp.cwd(ftp_base_dir)
+    dir_list = sorted(ftp.nlst())
+    print("Directories: " + str(dir_list))
+    excess_dirs = len(dir_list) - max_dirs
+
+    
+    if excess_dirs > 0:
+      excess_dir_list = dir_list[0:excess_dirs]
+      print("Removing directories" + str(excess_dir_list))
+      
+      for excess_dir in excess_dir_list:
+        
+        path=ftp_base_dir + excess_dir
+        
+        def delete_file(file_listing, path_to_file=path, ftp_inst_del=ftp_del):
+#          print("Trying to delete " + file_listing)
+#          print("Using ftp " + str(ftp))
+          file = path+"/"+file_listing.split()[-1]
+          cmd = "DELE " + file
+#          print(cmd)
+          ftp_inst_del.sendcmd(cmd) 
+
+        ftp.cwd(excess_dir)
+        print("Removing files in " + excess_dir)
+        ftp.retrlines("LIST", delete_file) # Blocks until callback finished.
+        ftp.cwd("..")
+        ftp.rmd(excess_dir)
+       
+  except:
+    """ Try again next time. """
+    traceback.print_exc()
+    ftp.close()
+except:
+  traceback.print_exc()
